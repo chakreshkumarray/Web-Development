2a52d4e81378499cd46a5b56ac63dfdc175054e2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-30 13:43:00 +0530

    57. Convert String to Integer without parse.js

---
+++ JavaScript/Tutorial/9. Structure Union Arrays/57. Convert String to Integer without parse.js
@@ -0,0 +1,8 @@
+// Convert string to integer without using parse int () function...
+
+function convertString()
+{
+  let a="100";
+  let b=Math.floor(a);
+  console.log(typeof(b));
+}convertString();
