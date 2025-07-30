8158ebc8994f137203144d9f4b712ba363c9c6d8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-30 13:38:07 +0530

    45.Arrays.js

---
+++ JavaScript/Tutorial/7. Arrays/45.Arrays.js
@@ -0,0 +1,26 @@
+// Create usiing Literal
+
+let a=[];
+document.write(a);
+
+let b=[10,20,30,40];
+document.write(b);
+
+// Create using new keyword constructor
+
+let a1=new array(10,20,30,40);
+document.write(a1);
+
+// Accessing element of array
+
+let a2=["HTML","CSS","JS"];
+document.write(a2[0]);
+document.write(a2[1]);
+document.write(a2[2
+]);
+
+// Accessing the first element of an array
+
+let a3=["HTML","CSS","JS","NodeJS"];
+let first=a3[0];
+document.write("first item is:",first);
