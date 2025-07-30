10588f827d6c02ca3e0c2851a82e8032ab1e82de
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-30 13:32:58 +0530

    Add files via upload

---
+++ JavaScript/Tutorial/4. Loops/20.While Loop.js
@@ -0,0 +1,14 @@
+let n=1;
+while(n<=10)
+{
+console.log(n)
+n++;
+}
+
+
+////////////////////
+let count = 1;
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
---
+++ JavaScript/Tutorial/4. Loops/21.Do While Loop.js
@@ -0,0 +1,14 @@
+let i=1;
+do{
+  console.log(i)
+  i++;
+}
+while(i<=8)
+
+/////////////
+let test = 1;
+do {
+  console.log(test);
+  test++;
+} 
+while(test<=5)
