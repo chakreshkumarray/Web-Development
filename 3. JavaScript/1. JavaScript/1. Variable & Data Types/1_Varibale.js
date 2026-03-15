5137c5c8e801ef74adcb1fdc7da8fc2a68fc1f5d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-15 22:03:06 +0530

    JavaScript

---
+++ 3. JavaScript/1. JavaScript/1. Variable & Data Types/1_Varibale.js
@@ -0,0 +1,13 @@
+const accountId = 54555
+let accountEmail = "ckray5503@gmail.com"
+var accountPassword = "123456"
+accountCity = "Sultanpur"
+
+// This is scope
+{
+
+}
+
+/*
+don't use var because scope issue only use let
+*/
