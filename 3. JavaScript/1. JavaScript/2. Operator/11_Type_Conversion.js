bebd6a3d7609937f6e27e00e7410ffcad11cb642
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-16 17:41:21 +0530

    Create 11_Type_Conversion.js

---
+++ 3. JavaScript/1. JavaScript/2. Operator/11_Type_Conversion.js
@@ -0,0 +1,11 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 1:09:56 time --> 
+let isLoggenIn = true
