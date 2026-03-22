ee34df7aa7f8178c7dfb65bb797fdc4b6df44b2c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-22 17:28:04 +0530

    Create 1_Number_Conversion.js

---
+++ 3. JavaScript/1. JavaScript/5. Data Type Conversion Confusion/1_Number_Conversion.js
@@ -0,0 +1,18 @@
+let score = undefined
+
+console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+33 -> 33
+"Ck" -> NaN
+"33Ck" -> NaN
+true -> 1
+null -> 0
+undefined -> NaN
+*/
