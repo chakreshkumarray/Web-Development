c34eeb02b5395f4b44a5215b01d35ab193f75261
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-22 17:27:44 +0530

    Create 3_Data_Types.js

---
+++ 3. JavaScript/1. JavaScript/4. Data Types and ECMA Standards/3_Data_Types.js
@@ -0,0 +1,22 @@
+let name = "Chakresh Kumar Ray"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+// number -> 12 to power 53
+// bigint
+// String = ""
+// boolean = true/false
+// null - standalone value, The Null type has exactly one value, called null.
+// undefined -> The Undefined type has exactly one value
+// symbol -> unique, The Symbol type is the set of all non-String values that may be used as the key of an Object property
+
+// object
+
+console.log(typeof "ck");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
