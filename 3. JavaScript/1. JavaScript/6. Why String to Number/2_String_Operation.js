95628ee95d23dd3a87ba616917955c2ba195ed5b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-22 17:28:17 +0530

    Create 2_String_Operation.js

---
+++ 3. JavaScript/1. JavaScript/6. Why String to Number/2_String_Operation.js
@@ -0,0 +1,11 @@
+let str1 = "Chakresh"
+let str2 = " Kumar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
