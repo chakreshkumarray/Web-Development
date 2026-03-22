1be580a9d19fd005323e9156a4f5341de82e3f9a
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-22 17:27:26 +0530

    Create 1_github.js

---
+++ 3. JavaScript/1. JavaScript/2. Save and Work on Github/1_github.js
@@ -0,0 +1,6 @@
+// 2. Save and Work on Github
+// i. Create a github Profile
+// ii. create a punlic repository
+// iii. Github use for version control on cloud provided by microsoft with vs code
+// iv. Go on github then click codespaces then you can write code on github
+
