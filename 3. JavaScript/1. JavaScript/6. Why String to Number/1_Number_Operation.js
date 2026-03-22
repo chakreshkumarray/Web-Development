1652b7a946c9d625e0ead8bbcdbf2d0a9d84c031
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-22 17:28:13 +0530

    Create 1_Number_Operation.js

---
+++ 3. JavaScript/1. JavaScript/6. Why String to Number/1_Number_Operation.js
@@ -0,0 +1,11 @@
+let value = 5503
+let negativeValue = -value;
+console.log(negativeValue)
+
+console.log(2 + 3)
+console.log(2 - 3)
+console.log(2 * 3)
+console.log(2 / 3)
+console.log(2 % 3)
+console.log(2 ** 3)
+
