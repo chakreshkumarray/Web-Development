e9a65d2a0061f04caf1bdc6ff3729d27833b3d8c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-24 13:08:54 +0530

    Update 3_Tricky_Conversion.js

---
+++ 3. JavaScript/1. JavaScript/6. Why String to Number/3_Tricky_Conversion.js
@@ -0,0 +1,13 @@
+console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log();
+
+let gameCounter = 100
+gameCounter++; // post fix - increment
+++gameCounter; // pre fix - increment
+console.log(gameCounter);
