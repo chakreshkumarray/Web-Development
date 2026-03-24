f0b5e7bdf4452fabeb26cb4053a369479845df46
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-24 13:09:06 +0530

    Create 1_Primitive_DataTypes.js

---
+++ 3. JavaScript/1. JavaScript/8. Data Types Summary/1_Primitive_DataTypes.js
@@ -0,0 +1,20 @@
+/**
+ * There are seven type
+ * String,
+ * Number,
+ * Boolean,
+ * null,
+ * undefined,
+ * symbol,
+ * BigInt
+ */
+
+const score = 45
+const socreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+console.log(typeof outsideTemp); // Object
+
+// javascript is dynamic type language
