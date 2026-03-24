efee6aba58097abd389f1e605fb8d07cf85343b0
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-24 13:09:27 +0530

    Create 2_Math.js

---
+++ 3. JavaScript/1. JavaScript/11. Number and Maths/2_Math.js
@@ -0,0 +1,56 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+/*
+
+abs: ƒ abs()
+acos: ƒ acos()
+acosh: ƒ acosh()
+asin: ƒ asin()
+asinh: ƒ asinh()
+atan: ƒ atan()
+atan2: ƒ atan2()
+atanh: ƒ atanh()
+cbrt: ƒ cbrt()
+ceil: ƒ ceil()
+clz32: ƒ clz32()
+cos: ƒ cos()
+cosh: ƒ cosh()
+exp: ƒ exp()
+expm1: ƒ expm1()
+f16round: ƒ f16round()
+floor: ƒ floor()
+fround: ƒ fround()
+hypot: ƒ hypot()
+imul: ƒ imul()
+log: ƒ log()
+log1p: ƒ log1p()
+log2: ƒ log2()
+log10: ƒ log10()
+max: ƒ max()
+min: ƒ min()
+pow: ƒ pow()
+random: ƒ random()
+round: ƒ round()
+sign: ƒ sign()
+sin: ƒ sin()
+sinh: ƒ sinh()
+sqrt: ƒ sqrt()
+tan: ƒ tan()
+tanh: ƒ tanh()
+trunc: ƒ trunc()
+*/
