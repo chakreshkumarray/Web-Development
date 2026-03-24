0d7648092125610875f147e2f68d6a0f076267cc
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-24 21:46:26 +0530

    Create 1_For_Of_Loop.js

---
+++ 3. JavaScript/1. JavaScript/28. High Order Array Loops/1_For_Of_Loop.js
@@ -0,0 +1,43 @@
+
+// For Of Loop
+
+// The for of loop is a new loop in ES6 that allows you to iterate over the values of an iterable object, such as an array, string, or map.
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+}
