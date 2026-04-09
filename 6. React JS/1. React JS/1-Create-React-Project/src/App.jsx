6845cfd45650121e6e41a04482fc3e05e6522c2e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-09 21:55:52 +0530

    Update App.jsx

--- 6. React JS/1. React JS/1-Create-React-Project/src/App.jsx
+++ 6. React JS/1. React JS/1-Create-React-Project/src/App.jsx
@@ -7,3 +7,15 @@ function App() {
 }
 
 export default App
+
+/*
+Single page Application
+This is a Library
+Doesn't follow folder structure
+Doesn't need to reload whole page(doc)
+Use DOM Component(Document Object Model) Reload
+Create a Copy of Actual React DOM
+Reuseing of Component
+Doesn't care how you handle routing, styling or state
+It's just follow convention not follow estrict rule 
+*/
