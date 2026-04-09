4ba8bc6ac939126e2a48ea0b0a015fdde50f1f3c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-09 18:54:37 +0530

    Create App.jsx

---
+++ 6. React JS/1. React JS/2-Understand-the-React-Flow-and-Structure/src/App.jsx
@@ -0,0 +1,14 @@
+import Chakresh from "./chakresh"
+
+function App() {
+
+  return (
+    <>
+    <Chakresh></Chakresh>
+    <h1>Chakresh Ray</h1>
+    <p>I am Chakresh</p>
+    </>
+  )
+}
+
+export default App
