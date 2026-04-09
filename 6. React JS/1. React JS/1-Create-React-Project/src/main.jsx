bd118cdffb0916232ef09efe3098139cdbee9626
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-09 18:53:51 +0530

    Update main.jsx

--- 6. React JS/1. React JS/1-Create-React-Project/src/main.jsx
+++ 6. React JS/1. React JS/1-Create-React-Project/src/main.jsx
@@ -8,3 +8,17 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// React
+// React-DOM-> Web
+// React-Native -> Mobile
+
+// npx / npm --> node package manager
+// create-react-app --> utility --> software
+// npx create-react-app --> old method
+// package,json--> entry point of react js --> provided some script to help run code or build UI, debugger, console, vite version, react, kernal
+// vite is a bundler
+
+// --> npm create vite@latest
+// -- npm install --> node_modules
+// --> npm run dev
