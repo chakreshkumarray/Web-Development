a36a0b84861882ca78dd39836df6e37176eadea2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-08-12 23:02:07 +0530

    Update main.jsx

--- 6. React JS/1. React JS/3-create-your-own-react-library-and react-jsx/1-React-Custom-JSX/src/main.jsx
+++ 6. React JS/1. React JS/3-create-your-own-react-library-and react-jsx/1-React-Custom-JSX/src/main.jsx
@@ -17,3 +17,9 @@ createRoot(document.getElementById('root')).render(
     <MyApp/>
   </StrictMode>,
 )
+
+/*
+
+--> In React JSX html used and in backend html convert in js object and then render in browser.
+
+*/
