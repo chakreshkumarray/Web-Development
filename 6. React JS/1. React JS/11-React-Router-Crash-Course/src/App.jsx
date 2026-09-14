1304f37305c95851198f78ca9798f2f588e019aa
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-09-14 22:24:27 +0530

    React Router

---
+++ 6. React JS/1. React JS/11-React-Router-Crash-Course/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      
+    </>
+  )
+}
+
+export default App
+
+/*
+Reat Router --> React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+*/
